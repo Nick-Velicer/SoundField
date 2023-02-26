b62d2ce3660cc34bd302695ed7c569aa55fe4659
Nick Velicer <njvelicer@ku.edu>
2023-02-26 17:18:23 -0600

    Electron/React adjustments

--- frontend/soundfield_app/src/App.js
+++ frontend/soundfield_app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState, useEffect} from "react";
+//import {useState, useEffect} from "react";
 
 //This is the main front end file, which renders
 //our current page as well as a dummy backend
@@ -7,8 +7,21 @@ import {useState, useEffect} from "react";
 
 //Last edited by Nick Velicer, Feb 11
 
-function App() {
+//temporary new function for trying new front end
+function TakeCSVForm(props) {
+  return (
+    <div style={{ textAlign: "center" }}>
+        <h1>Upload a csv file</h1>
+        <form>
+            <input type={"file"} accept={".csv"} />
+        </form>
+    </div>
+  );
+}
 
+//old front end
+function App() {
+  /*
   const [holder, setData] = useState([]);
 
   useEffect(() => {
@@ -19,34 +32,12 @@ function App() {
     const response = await fetch("http://localhost:8000/api/v2/");
     setData(await response.json());
   }
+  */
 
   //Main return statement for the actual html
   return (
     <div id="root">
-      <h1 className="title">SoundField</h1>
-      <h2 className="subtitle">Creating art from the active brain.</h2>
-      <h2 style={{marginLeft: "9%"}}>_____________________________________</h2>
-      <div id="columns" style={{width: "100%"}}>
-          <div id="column1">
-              <h1 style={{marginBottom: "20px"}}>Our Data:</h1>
-              <video src={"Videos/EEGBaseAnimation.mp4"} width="600" height="200" autoPlay={true} muted={true} type="video/mp4"/>
-              <h1>Our Team:</h1>
-              <h2>Anna Burns</h2>
-              <h2>Davis Johnson</h2>
-              <h2>Zach Misic</h2>
-              <h2>Michael Perry</h2>
-              <h2>Nick Velicer</h2>
-          </div>
-          <div id="column2" style={{float: "right", marginRight: "10%"}}>
-              <h1 style={{marginTop: "-2px"}}>Our Art:</h1>
-              <video src={"Videos/harmonograph.mp4"} width="480" height="480" autoPlay={true} muted={true} type="video/mp4"/>
-          </div>
-      </div>
-      <p style={{color: "white"}}>Temporary API testing:</p>
-      <ol>
-        <li>{holder.idNum}</li>
-        <li>{holder.ch1}</li>
-      </ol>
+      <TakeCSVForm></TakeCSVForm>
     </div>
   );
 }
