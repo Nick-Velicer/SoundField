4921a982b71a8b88686259f0ec5a264870368bc6
Davis Johnson <89803504+Davis-K-Johnson@users.noreply.github.com>
2023-04-23 22:34:24 -0500

    about us section added

--- frontend/soundfield_app/src/AboutUs.js
+++ frontend/soundfield_app/src/AboutUs.js
@@ -11,6 +11,14 @@ function AboutUs() {
         <h2>Zach Misic</h2>
         <h2>Michael Perry</h2>
         <h2>Nick Velicer</h2>
+        <h4><br></br>Hello, we're Soundfield, a small team of 5 students working
+        on our capstone project at Kansas University. We've designed an application
+        that aims to visualize your brain activity while listening to music. The
+        program is a marriage of Code, Art, and Neuroscience where we use generative
+        art to make unique creations based on your thoughts while listening to
+        different kinds of music. We take input from a CSV document generated from
+        14-channel EEG headset and create artwork based on the thought and emotional
+        patterns that are recorded there.</h4>
       </div>
     </div>
   );
