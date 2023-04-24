856ecdd1d0555046ce1e6ed1def7bdf0b30ddb1e
Nick Velicer <njvelicer@ku.edu>
2023-04-23 23:59:03 -0500

    message edits

--- frontend/soundfield_app/src/RenderSection.js
+++ frontend/soundfield_app/src/RenderSection.js
@@ -4,7 +4,9 @@ function RenderSection() {
   
   return (
     <div>
-      <p>Your rendering will appear below once complete:</p>
+      <p>Your rendering will appear below once complete.</p>
+      <p>If it fails to import here, the final video can also be found at:</p>
+      <p>*Soundfield Directory*/frontend/soundfield_app/src/output.mp4</p>
       <video src="output.mp4" autoplay muted loop/>
     </div>
   );
